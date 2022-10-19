5f3b9ee071e3c58eb7fa0a0eb974bd380d74462d
fabiandriessen <73178176+fabiandriessen@users.noreply.github.com>
2022-10-19 12:21:20 +0200

    Last runs consistent resolution

--- ABM/own_work/new_batchrunner.py
+++ ABM/own_work/new_batchrunner.py
@@ -23,18 +23,16 @@ G = pickle.load(open('data/network.p', 'rb'))
 paths = pickle.load(open('data/paths.p', 'rb'))
 df_random = pickle.load(open('data/df_random.p', 'rb'))
 
-
 for i, row in df_9scenarios.iterrows():
+    # df_abm = create_input_data_abm(G, paths, row['non_zero_flows'], row['optimal_facilities'])
+    # # configure df random for abm
+    # df_random['key'] = df_random.apply(lambda x: create_key(x.origin, x.destination, x.route_v), axis=1)
+    # df_random = df_random.loc[df_random.key.isin(row['non_zero_flows'].keys())]
+    # df_random = df_random.loc[df_random.trip_count != 0]
     #
-    #     df_abm = create_input_data_abm(G, paths, row['non_zero_flows'], row['optimal_facilities'])
-    #     # configure df random for abm
-    #     df_random['key'] = df_random.apply(lambda x: create_key(x.origin, x.destination, x.route_v), axis=1)
-    #     df_random = df_random.loc[df_random.key.isin(row['non_zero_flows'].keys())]
-    #     df_random = df_random.loc[df_random.trip_count != 0]
-    #
-    #     pickle.dump(df_random, open('data/inputs/df_random_batch' + str(i) + '.p', 'wb'))
-    #     df_abm.to_csv('data/inputs/df_abm_batch' + str(i) + '.csv')
-    #     pickle.dump(row['non_zero_flows'], open('data/inputs/non_zero_flows_batch' + str(i) + '.p', 'wb'))7
+    # pickle.dump(df_random, open('data/inputs/df_random_batch' + str(i) + '.p', 'wb'))
+    # df_abm.to_csv('data/inputs/df_abm_batch' + str(i) + '.csv')
+    # pickle.dump(row['non_zero_flows'], open('data/inputs/non_zero_flows_batch' + str(i) + '.p', 'wb'))
     # print(i)
     # print(i)
     params = {'c': row['c'], 'r': row['r'], 'run': i, 'seed': seeds}
@@ -49,4 +47,5 @@ for i, row in df_9scenarios.iterrows():
                            number_processes=20,
                            display_progress=True)
 
-        pickle.dump(result, open('results/batch_9scenarios'+str(row['r'])+str(row['c'])+str(row['m'])+'.p', 'wb'))
+        pickle.dump(result,
+                    open('results/batch_9scenarios' + str(row['r']) + str(row['c']) + str(row['m']) + '.p', 'wb'))
